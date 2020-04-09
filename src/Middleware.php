3682bee6859d0e9f74a5bde9fc37faeacd89e654
Bosince <mt4office@mail.com>
2020-04-09 17:47:46 +0800

    bugfix for config method.

--- src/Middleware.php
+++ src/Middleware.php
@@ -16,12 +16,15 @@ class Middleware
      */
     public function handle($request, Closure $next)
     {
+        $default= Config::get('app.locale');
         $conf   = Config::get('admini');
-        $lang   = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE'])[0] : config('app.locale');
+        $lang   = isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])
+                  ? explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE'])[0]
+                  : $default;
         $auto   = $conf['auto_language'] ? $lang : null;
         $locale = $_REQUEST['lang'] ?? session('locale', $auto);
         $locale = collect($conf['languages'])->firstWhere('locale', $locale);
-        $locale = $locale['locale'] ?? Config::get('app.locale');
+        $locale = $locale['locale'] ?? $default;
 
         session(['locale' => $locale]);
         \App::setLocale($locale);
