655e958252aaa8b4daa7625f785bf38d71b9237f
SUNGMEE <mo@lxooo.com>
2020-06-16 16:57:57 +0800

    chang session to cookie

--- src/Middleware.php
+++ src/Middleware.php
@@ -22,17 +22,22 @@ class Middleware
                   ? explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE'])[0]
                   : $default;
         $auto   = $conf['auto_language'] ? $lang : null;
-        $locale = $request->lang ?? session('locale', $auto);
+        $locale = $request->lang ?? $request->cookie('locale') ?? session('locale', $auto);
         $locale = collect($conf['languages'])->firstWhere('locale', $locale);
         $locale = $locale['locale'] ?? $default;
 
         session(['locale' => $locale]);
         \App::setLocale($locale);
 
-        $params = $request->all();
-        unset($params['lang']);
-        $url = url()->current() . '?' . http_build_query($params);
+        if ($request->has('lang')) {
+            $params  = $request->all(); unset($params['lang']);
+            $params  = http_build_query($params);
+            $params  = empty($params) ? '' : "?$params";
+            $url     = url()->current() . $params;
+            $minutes = 60 * 24 * 7;
+            return redirect($url)->cookie('locale', $locale, $minutes);
+        }
 
-        return $request->has('lang') ? redirect($url) : $next($request);
+        return $next($request);
     }
 }
